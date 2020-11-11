2ef666967d4edf8c5bfb11bca761dfb74bb72fa2
Augusto Batista <augusto+github@herrmann.tech>
2020-11-11 16:05:08 -0300

    inicia a implementação da autenticação com hash

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from typing import List, Optional
 from pydantic import BaseModel
@@ -31,11 +33,6 @@ fake_users_db = {
 def fake_hash_password(password: str):
     return "fakehashed" + password
 
-def fake_decode_token(token):
-    return User(
-        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
-    )
-
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 class User(BaseModel):
@@ -47,10 +44,26 @@ class User(BaseModel):
 class UserInDB(User):
     hashed_password: str
 
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
 
 def fake_decode_token(token):
     # This doesn't provide any security at all
