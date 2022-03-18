cac646f0cb8be23bed39b6d7db4ad2f40aece64c
Augusto Herrmann <augusto+github@herrmann.tech>
2022-03-18 15:19:05 -0300

    Remove line feed

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import os
 
 from sqlalchemy import create_engine
-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import event, DDL
