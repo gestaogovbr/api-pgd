1b4545d6a92bf5853c65d26e0965318ab329a313
Augusto Herrmann <augusto+github@herrmann.tech>
2023-08-18 17:02:20 -0300

    Implement adding a redirect uri through fief's api
    Co-Authored-By:  Marcelo de Lima <marcelo.delima.brazil@gmail.com>

--- src/fief_admin.py
+++ src/fief_admin.py
@@ -5,11 +5,12 @@ import urllib
 
 import httpx
 
+
 class FiefAdminHelper:
     """
     A helper class for interacting with the Fief API using admin authentication.
 
-    Args:
+    Attributes:
         api_token (str): The API token for admin authentication.
         base_url (str): The base URL for the Fief API.
 
@@ -44,17 +45,17 @@ class FiefAdminHelper:
             data (dict, optional): Dictionary to post. Defaults to None.
 
         Returns:
-            httpx.Response: The Response object returned by httpx.
+            httpx.Response: The Response object returned by the API call.
         """
         url = f"{self.base_url}/admin/api/{local_url}"
         if params:
             url += f"?{urllib.parse.urlencode(params)}"
         headers = {
-            "accept": "application/json",
-            "authorization": f"Bearer {self.api_token}",
+            "Accept": "application/json",
+            "Authorization": f"Bearer {self.api_token}",
         }
-        if method in ["POST", "PUT"]:
-            headers["content-type"] = "application/json"
+        if method in ["PATCH", "POST", "PUT"]:
+            headers["Content-Type"] = "application/json"
             return httpx.request(
                 method=method,
                 url=url,
@@ -68,32 +69,32 @@ class FiefAdminHelper:
         )
 
     @cached_property
-    def first_tenant(self) -> str:
+    def first_tenant(self) -> dict:
         """
-        Get the tenant id for the first tenant registered in Fief.
+        Get data for the first tenant registered in Fief.
 
         Returns:
-            str: The tenant id.
+            dict: The tenant data returned by the Fief admin API.
         """
         return self.fief_admin_call(
             method="GET",
             local_url="tenants/",
             params={"limit": 1, "skip": 0},
-        ).json()["results"][0]["id"]
+        ).json()["results"][0]
 
     @cached_property
-    def first_client(self) -> str:
+    def first_client(self) -> dict:
         """
-        Get the client id for the first client registered in Fief.
+        Get data for the first client registered in Fief.
 
         Returns:
-            str: The client id.
+            dict: The client data returned by the Fief admin API.
         """
         return self.fief_admin_call(
             method="GET",
             local_url="clients/",
-            params={"tenant": self.first_tenant},
-        ).json()["results"][0]["id"]
+            params={"tenant": self.first_tenant["id"]},
+        ).json()["results"][0]
 
     def register_user(
         self, email: str, password: str, cod_unidade: int, is_superuser: bool = False
@@ -107,7 +108,7 @@ class FiefAdminHelper:
             cod_unidade (int): User's organizational unit code.
 
         Returns:
-            httpx.Response: The Response object returned by httpx.
+            httpx.Response: The Response object returned by API call.
         """
         fields = {"cod_unidade": cod_unidade}
         data = {
@@ -117,11 +118,62 @@ class FiefAdminHelper:
             "is_superuser": is_superuser,
             "is_verified": False,
             "fields": fields,
-            "tenant_id": self.first_tenant,
+            "tenant_id": self.first_tenant["id"],
         }
         return self.fief_admin_call(
             method="POST",
             local_url="users/",
+            data=data,
+        )
+
+    def get_client(self, client_id: str) -> dict:
+        """Obtain client data from the Fief admin API.
+
+        Args:
+            client_id (str): the client's internal id.
+        
+        Returns:
+            dict: the client data returned by the Fief admin API.
+        """
+        return self.fief_admin_call(
+            method="GET",
+            local_url=f"clients/{client_id}",
+        ).json()
+
+    def client_add_redirect_uri(
+        self, uri: str, client_id: str = None
+    ) -> httpx.Response:
+        """Adds a Redirect URI to a Fief client.
+
+        Args:
+            uri (str): the redirect URI to be added.
+            client_id (str, optional): the client's internal id. If None
+            or omitted, will use the first client found in the API.
+            Defaults to None.
+
+        Returns:
+            httpx.Response: the Response object obtained from the API
+                call.
+        """
+        if not client_id:
+            client_id = self.first_client["id"]
+        client = self.get_client(client_id)
+        redirect_uris = client["redirect_uris"]
+
+        # remove http URIs that are not localhost (will be rejected by
+        # Fief's API otherwise)
+        for uri_string in redirect_uris:
+            existing_uri = urllib.parse.urlparse(uri_string)
+            if existing_uri.scheme == "http" and existing_uri.hostname != "localhost":
+                redirect_uris.remove(uri_string)
+
+        redirect_uris.append(uri)
+        data = {
+            "redirect_uris": redirect_uris,
+        }
+        return self.fief_admin_call(
+            method="PATCH",
+            local_url=f"clients/{client_id}",
             data=data,
         )
 
@@ -134,7 +186,7 @@ class FiefAdminHelper:
         ask_at_registration: bool = False,
         ask_at_update: bool = False,
         required: bool = False,
-    ):
+    ) -> httpx.Response:
         """Creates a user field in Fief.
 
         See documentation at
@@ -155,6 +207,10 @@ class FiefAdminHelper:
             required (bool, optional): Whether or not the field is required
                 when presented in forms.
                 Defaults to False.
+
+         Returns:
+            httpx.Response: the Response object obtained from the API
+                call.
         """
         data = {
             "name": name,
