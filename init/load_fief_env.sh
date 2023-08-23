5fd2ae962b13520f6f00016c4a1baaa1d10fa8e1
Augusto Herrmann <augusto+github@herrmann.tech>
2023-08-23 15:50:38 -0300

    Generate API key on setup
    Co-Authored-By: Gustavo Silveira <gustavo.silveira@economia.gov.br>
    Co-Authored-By:Marcelo de Lima <marcelo.delima.brazil@gmail.com>

--- init/load_fief_env.sh
+++ init/load_fief_env.sh
@@ -12,10 +12,13 @@ else
     docker run ghcr.io/fief-dev/fief:latest fief quickstart --docker "$@" | tee -i .env_fief
 fi
 
+# Generate admin API key
+docker-compose run web python -c "import secrets; print(secrets.token_urlsafe())" > .fief_api_key
+
 # Prepare the dest vars
 source_file=".env_fief"
-template_file=".env"
-variables=("CLIENT_ID" "CLIENT_SECRET" "ENCRYPTION_KEY" "SECRET" "MAIN_USER_EMAIL" "MAIN_USER_PASSWORD")
+output_file=".env"
+variables=("CLIENT_ID" "CLIENT_SECRET" "ENCRYPTION_KEY" "SECRET" "MAIN_USER_EMAIL" "MAIN_USER_PASSWORD" "MAIN_ADMIN_API_KEY")
 
 # Initialize an associative array to store the variable values
 declare -A var_values
@@ -29,10 +32,15 @@ for var in "${variables[@]}"; do
     fi
 done
 
+# Replace value of FIEF API key with generated key
+fief_api_key=$(cat .fief_api_key)
+var_values["MAIN_ADMIN_API_KEY"]="$fief_api_key"
+
 # Replace the extracted variable values
 for var in "${variables[@]}"; do
-    sed -i "s/FIEF_$var=.*/FIEF_$var=${var_values[$var]}/" $template_file
+    sed -i "s/FIEF_$var=.*/FIEF_$var=${var_values[$var]}/" $output_file
 done
 
 # Remove tmp file
 rm .env_fief
+rm .fief_api_key
