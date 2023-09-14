69e611f85b9f4cbc3a23a931b145119f0bccad23
Augusto Herrmann <augusto+github@herrmann.tech>
2023-09-14 11:40:13 -0300

    Create model for Contribuicao

--- src/models.py
+++ src/models.py
@@ -2,20 +2,32 @@
 banco pelo ORM do SQLAlchemy.
 """
 
-from sqlalchemy import (Boolean, Column, ForeignKey,
-                        Integer, BigInteger,String, Float, Date, DateTime,
-                        UniqueConstraint)
+from sqlalchemy import (
+    Boolean,
+    Column,
+    ForeignKey,
+    Integer,
+    BigInteger,
+    String,
+    Float,
+    Date,
+    DateTime,
+    UniqueConstraint,
+)
 from sqlalchemy import event, DDL
 from sqlalchemy.orm import relationship
 
 from users import Base
+
 
 class PlanoTrabalho(Base):
     "Plano de Trabalho"
     __tablename__ = "plano_trabalho"
     cod_SIAPE_instituidora = Column(Integer, primary_key=True, index=True)
     id_plano_trabalho_participante = Column(Integer, primary_key=True, index=True)
-    id_plano_entrega_unidade = Column(Integer, ForeignKey("plano_entregas.id_plano_entrega_unidade"))
+    id_plano_entrega_unidade = Column(
+        Integer, ForeignKey("plano_entregas.id_plano_entrega_unidade")
+    )
     cod_SIAPE_unidade_exercicio = Column(Integer)
     cpf_participante = Column(Integer, ForeignKey("participante.cpf_participante"))
     data_inicio_plano = Column(Date)
@@ -31,13 +43,25 @@ class PlanoTrabalho(Base):
         back_populates="plano_trabalho",
         lazy="joined",
         passive_deletes=True,
-        cascade="save-update, merge, delete, delete-orphan"
+        cascade="save-update, merge, delete, delete-orphan",
     )
-    __table_args__ = (UniqueConstraint(
-        "cod_SIAPE_instituidora",
-        "id_plano_trabalho_participante",
-        name="_instituidora_plano_trabalho_uc"
-    ),)
+    __table_args__ = (
+        UniqueConstraint(
+            "cod_SIAPE_instituidora",
+            "id_plano_trabalho_participante",
+            name="_instituidora_plano_trabalho_uc",
+        ),
+    )
+
+
+class Contribuicao(Base):
+    "Contribuição para um Plano de Trabalho"
+    __tablename__ = "contribuicao"
+    id = Column(Integer, primary_key=True)
+    id_plano_trabalho_participante = Column(
+        Integer, ForeignKey("plano_trabalho.id_plano_trabalho_participante")
+    )
+
 
 # trigger = DDL("""
 #     CREATE TRIGGER inseredata_trigger
