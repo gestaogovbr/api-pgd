80a2e48be72fb70416aa2356b04cb0749c18e605
Augusto Herrmann <augusto+github@herrmann.tech>
2023-09-14 14:53:59 -0300

    Add model for Contribuicao

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 """Definições dos modelos de dados da API que serão persistidos no
 banco pelo ORM do SQLAlchemy.
 """
+import enum
 
 from sqlalchemy import (
     Boolean,
@@ -12,6 +13,7 @@ from sqlalchemy import (
     Float,
     Date,
     DateTime,
+    Enum,
     UniqueConstraint,
 )
 from sqlalchemy import event, DDL
@@ -23,21 +25,24 @@ from users import Base
 class PlanoTrabalho(Base):
     "Plano de Trabalho"
     __tablename__ = "plano_trabalho"
-    cod_SIAPE_instituidora = Column(Integer, primary_key=True, index=True)
-    id_plano_trabalho_participante = Column(Integer, primary_key=True, index=True)
-    id_plano_entrega_unidade = Column(
-        Integer, ForeignKey("plano_entregas.id_plano_entrega_unidade")
+    cod_SIAPE_instituidora = Column(
+        Integer, primary_key=True, index=True, nullable=False
     )
-    cod_SIAPE_unidade_exercicio = Column(Integer)
-    cpf_participante = Column(Integer, ForeignKey("participante.cpf_participante"))
-    data_inicio_plano = Column(Date)
-    data_termino_plano = Column(Date)
-    carga_horaria_total_periodo_plano = Column(Integer)
-    # situacao = Column(String)
-    # cod_unidade = Column(BigInteger, nullable=False)
-    # cod_plano = Column(String, nullable=False)
+    id_plano_trabalho_participante = Column(
+        Integer, primary_key=True, index=True, nullable=False
+    )
+    id_plano_entrega_unidade = Column(
+        Integer, ForeignKey("plano_entregas.id_plano_entrega_unidade"), nullable=False
+    )
+    cod_SIAPE_unidade_exercicio = Column(Integer, nullable=False)
+    cpf_participante = Column(
+        Integer, ForeignKey("participante.cpf_participante"), nullable=False
+    )
+    data_inicio_plano = Column(Date, nullable=False)
+    data_termino_plano = Column(Date, nullable=False)
+    carga_horaria_total_periodo_plano = Column(Integer, nullable=False)
     data_atualizacao = Column(DateTime)
-    data_insercao = Column(DateTime)
+    data_insercao = Column(DateTime, nullable=False)
     contribuicoes = relationship(
         "Contribuicao",
         back_populates="plano_trabalho",
@@ -54,13 +59,27 @@ class PlanoTrabalho(Base):
     )
 
 
+class TipoContribuicao(enum.Enum):
+    entrega_propria_unidade = 1
+    nao_vinculada = 2
+    entrega_outra_unidade = 3
+
+
 class Contribuicao(Base):
     "Contribuição para um Plano de Trabalho"
     __tablename__ = "contribuicao"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     id_plano_trabalho_participante = Column(
-        Integer, ForeignKey("plano_trabalho.id_plano_trabalho_participante")
+        Integer,
+        ForeignKey("plano_trabalho.id_plano_trabalho_participante"),
+        nullable=False,
     )
+    tipo_contribuicao = Column(Integer, Enum(TipoContribuicao), nullable=False)
+    descricao_contribuicao = Column(String)
+    id_entrega = Column(Integer, ForeignKey("Entregas.id_entrega"), nullable=False)
+    horas_vinculadas = Column(Integer, nullable=False)
+    data_atualizacao = Column(DateTime)
+    data_insercao = Column(DateTime, nullable=False)
 
 
 # trigger = DDL("""
