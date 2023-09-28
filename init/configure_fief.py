213b4f7aec06185a2c6a23e8c5c0016819ef1583
Augusto Herrmann <augusto+github@herrmann.tech>
2023-09-28 16:10:56 -0300

    Print debug messages to log in fief-config

--- init/configure_fief.py
+++ init/configure_fief.py
@@ -23,28 +23,33 @@ if not hostname:
     raise ValueError("'WEB_HOST_NAME' environment variable must be set.")
 port = os.environ.get("WEB_PORT")
 
+# Add redirect URI
 uri = f"{scheme}://{hostname}:{port}/docs/oauth2-redirect"
-print("-------------------")
-print(uri)
-
+print(f"Adding redirect URI: {uri}...")
 response = fief_admin.client_add_redirect_uri(
     # uri=f"{scheme}://{hostname}:{port}/docs/oauth2-redirect"
     uri=uri
 )
+print(f"Status: {response.status}")
 response.raise_for_status()
 
 # Add custom user fields
+print("Creating user field: {admin_user}...")
 response = fief_admin.create_user_field(
     name="Código SIAPE da unidade instituidora",
     slug="cod_SIAPE_instituidora",
     field_type="INTEGER",
     default_value=0,
 )
+print(f"Status: {response.status}")
 response.raise_for_status()
 
 # Set superuser field for the admin user
+admin_user = os.environ.get("FIEF_MAIN_USER_EMAIL")
+print(f"Setting superuser flag for user: {admin_user}...")
 response = fief_admin.patch_user(
-    email=os.environ.get("FIEF_MAIN_USER_EMAIL"),
+    email=admin_user,
     data={"is_superuser": True}
 )
+print(f"Status: {response.status}")
 response.raise_for_status()
