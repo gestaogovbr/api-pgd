ab600cf8df6460012a5a8b06412205781bd5c4c2
Augusto Herrmann <augusto+github@herrmann.tech>
2024-01-23 15:55:05 -0300

    Remove unused import

--- src/schemas.py
+++ src/schemas.py
@@ -6,7 +6,7 @@ Pydantic: https://docs.pydantic.dev/2.0/
 """
 
 from typing import List, Optional
-from datetime import date, datetime
+from datetime import date
 
 from pydantic import BaseModel, ConfigDict, Field, EmailStr
 from pydantic import model_validator, field_validator
