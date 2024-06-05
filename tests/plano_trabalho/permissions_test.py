d2fe63527d4438a8f392fb2092b4b4bd592d136c
Augusto Herrmann <augusto+github@herrmann.tech>
2024-06-05 17:58:38 -0300

    Refactor plano_trabalho permissions test into a class

--- tests/plano_trabalho/permissions_test.py
+++ tests/plano_trabalho/permissions_test.py
@@ -1,55 +1,74 @@
-"""Testes relacionados às permissões de uso dos endpoints do Plano de
-Trabalho.
-"""
-
+import pytest
 from httpx import Client
 from fastapi import status
 
+from .core_test import assert_equal_plano_trabalho
 
-def test_get_pt_different_unit(
-    input_pt: dict,
-    header_usr_2: dict,
-    truncate_pt,  # pylint: disable=unused-argument
-    truncate_pe,  # pylint: disable=unused-argument
-    example_pe_unidade_3,  # pylint: disable=unused-argument
-    example_pt_unidade_3,  # pylint: disable=unused-argument
-    client: Client,
-):
-    """Tenta acessar um plano de trabalho de uma unidade diferente, à
-    qual o usuário não tem acesso."""
-    response = client.get(
-        "/organizacao/SIAPE/3"  # Sem autorização nesta unidade
-        f"/plano_trabalho/{input_pt['id_plano_trabalho']}",
-        headers=header_usr_2,
-    )
+class TestGetPlanoDeTrabalhoDiferenteUnidade:
+    """
+    Testes para verificar o acesso a um plano de trabalho de uma unidade diferente.
 
-    assert response.status_code == status.HTTP_403_FORBIDDEN
+    Esses testes verificam o comportamento do sistema quando um usuário tenta acessar
+    um plano de trabalho de uma unidade diferente, tanto com um usuário sem permissão
+    quanto com um usuário com permissão de administrador.
+    """
 
+    @pytest.fixture
+    def setup(
+        self,
+        input_pt: dict,
+        header_usr_2: dict,
+        header_admin: dict,
+        truncate_pt,  # pylint: disable=unused-argument
+        truncate_pe, # pylint: disable=unused-argument
+        example_pe_unidade_3,  # pylint: disable=unused-argument
+        example_pt_unidade_3,  # pylint: disable=unused-argument
+        client: Client,
+    ):
+        """
+        Fixture de configuração para os testes da classe
+        TestGetPlanoDeTrabalhoDiferenteUnidade.
 
-def test_get_pt_different_unit_admin(
-    input_pt: dict,
-    header_admin: dict,
-    truncate_pt,  # pylint: disable=unused-argument
-    truncate_pe,  # pylint: disable=unused-argument
-    example_pe_unidade_3,  # pylint: disable=unused-argument
-    example_pt_unidade_3,  # pylint: disable=unused-argument
-    client: Client,
-):
-    """Tenta acessar um plano de trabalho de uma unidade diferente, mas
-    com um usuário com permissão de admin."""
-    response = client.get(
-        f"/organizacao/SIAPE/3"  # Unidade diferente
-        f"/plano_trabalho/{input_pt['id_plano_trabalho']}",
-        headers=header_admin,
-    )
+        Esta fixture inicializa os atributos da classe com os valores
+        fornecidos pelos parâmetros.
+        """
+        # pylint: disable=attribute-defined-outside-init
+        self.input_pt = input_pt
+        self.header_usr_2 = header_usr_2
+        self.header_admin = header_admin
+        self.client = client
 
-    # Inclui os campos de resposta do json que não estavam no template
-    input_pt["status"] = 3
-    input_pt["cod_unidade_executora"] = 3
-    input_pt["carga_horaria_disponivel"] = input_pt["carga_horaria_disponivel"]
-    input_pt["avaliacao_registros_execucao"][0][
-        "data_avaliacao_registros_execucao"
-    ] = "2023-01-03"
+    def test_get_pt_different_unit(self, setup):  # pylint: disable=unused-argument
+        """
+        Tenta acessar um plano de trabalho de uma unidade diferente, à
+        qual o usuário não tem acesso.
+        """
+        response = self.client.get(
+            "/organizacao/SIAPE/3"  # Sem autorização nesta unidade
+            f"/plano_trabalho/{self.input_pt['id_plano_trabalho']}",
+            headers=self.header_usr_2,
+        )
 
-    assert response.status_code == status.HTTP_200_OK
-    assert_equal_plano_trabalho(response.json(), input_pt)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_get_pt_different_unit_admin(self, setup):  # pylint: disable=unused-argument
+        """
+        Tenta acessar um plano de trabalho de uma unidade diferente, mas
+        com um usuário com permissão de admin.
+        """
+        response = self.client.get(
+            f"/organizacao/SIAPE/3"  # Unidade diferente
+            f"/plano_trabalho/{self.input_pt['id_plano_trabalho']}",
+            headers=self.header_admin,
+        )
+
+        # Inclui os campos de resposta do json que não estavam no template
+        self.input_pt["status"] = 3
+        self.input_pt["cod_unidade_executora"] = 3
+        self.input_pt["carga_horaria_disponivel"] = self.input_pt["carga_horaria_disponivel"]
+        self.input_pt["avaliacao_registros_execucao"][0][
+            "data_avaliacao_registros_execucao"
+        ] = "2023-01-03"
+
+        assert response.status_code == status.HTTP_200_OK
+        assert_equal_plano_trabalho(response.json(), self.input_pt)
