28e4e9cb36eeec47dec5759ef4ad634acf041750
Augusto Herrmann <augusto+github@herrmann.tech>
2024-06-06 16:35:45 -0300

    Refactor into get_pt method, move test to permissions_test

--- tests/plano_trabalho/permissions_test.py
+++ tests/plano_trabalho/permissions_test.py
@@ -5,13 +5,15 @@ from fastapi import status
 from .core_test import BasePTTest
 
 
-class TestGetPlanoDeTrabalhoDiferenteUnidade(BasePTTest):
+class TestPlanoDeTrabalhoDiferenteUnidade(BasePTTest):
     """
-    Testes para verificar o acesso a um plano de trabalho de uma unidade diferente.
+    Testes para verificar o acesso a um plano de trabalho de uma unidade
+    diferente.
 
-    Esses testes verificam o comportamento do sistema quando um usuário tenta acessar
-    um plano de trabalho de uma unidade diferente, tanto com um usuário sem permissão
-    quanto com um usuário com permissão de administrador.
+    Esses testes verificam o comportamento do sistema quando um usuário
+    tenta acessar ou criar um plano de trabalho de uma unidade diferente,
+    tanto com um usuário sem permissão quanto com um usuário com
+    permissão de administrador.
     """
 
     def test_get_pt_different_unit(
@@ -24,13 +26,15 @@ class TestGetPlanoDeTrabalhoDiferenteUnidade(BasePTTest):
         Tenta acessar um plano de trabalho de uma unidade diferente, à
         qual o usuário não tem acesso.
         """
-        response = self.client.get(
-            "/organizacao/SIAPE/3"  # Sem autorização nesta unidade
-            f"/plano_trabalho/{self.input_pt['id_plano_trabalho']}",
-            headers=header_usr_2,
+        response = self.get_pt(
+            id_plano_trabalho=self.input_pt["id_plano_trabalho"],
+            cod_unidade_autorizadora=3,  # Unidade diferente
+            header_usr=header_usr_2
         )
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
+        detail_message = "Usuário não tem permissão na cod_unidade_autorizadora informada"
+        assert detail_message in response.json().get("detail")
 
     def test_get_pt_different_unit_admin(
         self,
@@ -42,10 +46,9 @@ class TestGetPlanoDeTrabalhoDiferenteUnidade(BasePTTest):
         Tenta acessar um plano de trabalho de uma unidade diferente, mas
         com um usuário com permissão de admin.
         """
-        response = self.client.get(
-            f"/organizacao/SIAPE/3"  # Unidade diferente
-            f"/plano_trabalho/{self.input_pt['id_plano_trabalho']}",
-            headers=header_admin,
+        # Obter o plano de trabalho de uma unidade diferente
+        response = self.get_pt(
+            self.input_pt["id_plano_trabalho"], 3, header_usr=header_admin
         )
 
         # Inclui os campos de resposta do json que não estavam no template
@@ -61,3 +64,17 @@ class TestGetPlanoDeTrabalhoDiferenteUnidade(BasePTTest):
 
         assert response.status_code == status.HTTP_200_OK
         self.assert_equal_plano_trabalho(response.json(), input_pt)
+
+def test_create_plano_trabalho_unidade_nao_permitida(self, header_usr_2):
+    """Tenta criar um novo Plano de Trabalho do Participante em uma
+    organização na qual ele não está autorizado.
+    """
+    # Criar o Plano de Trabalho em uma unidade diferente
+    response = self.create_pt(
+        self.input_pt, cod_unidade_autorizadora=3, header_usr=header_usr_2
+    )
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    detail_message = "Usuário não tem permissão na cod_unidade_autorizadora informada"
+    assert detail_message in response.json().get("detail")
+
