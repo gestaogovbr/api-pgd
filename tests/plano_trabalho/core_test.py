2ee0c7575becb00932695c1c35d4d10d9a92cfe0
Augusto Herrmann <augusto+github@herrmann.tech>
2024-06-06 17:45:28 -0300

    Add test for checking valid percentual_contribuicao

--- tests/plano_trabalho/core_test.py
+++ tests/plano_trabalho/core_test.py
@@ -390,64 +390,6 @@ class TestPlanoDeTrabalhoDatasEntrega(BasePTTest):
             assert response.status_code == status.HTTP_201_CREATED
 
 
-@pytest.mark.parametrize("omitted_fields", enumerate(FIELDS_CONTRIBUICAO["optional"]))
-def test_create_plano_trabalho_contribuicao_omit_optional_fields(
-    # fixture example_pe é necessária para cumprir IntegrityConstraint (FK)
-    truncate_pe,  # pylint: disable=unused-argument
-    example_pe,  # pylint: disable=unused-argument
-    input_pt: dict,
-    omitted_fields: list,
-    user1_credentials: dict,
-    header_usr_1: dict,
-    client: Client,
-):
-    """Tenta criar um novo plano de trabalho omitindo campos opcionais"""
-    partial_input_pt = input_pt.copy()
-    offset, field_list = omitted_fields
-    for field in field_list:
-        for contribuicao in partial_input_pt["contribuicoes"]:
-            if field in contribuicao:
-                del contribuicao[field]
-
-    partial_input_pt["id_plano_trabalho"] = 557 + offset
-    response = client.put(
-        f"/organizacao/SIAPE/{user1_credentials['cod_unidade_autorizadora']}"
-        f"/plano_trabalho/{partial_input_pt['id_plano_trabalho']}",
-        json=partial_input_pt,
-        headers=header_usr_1,
-    )
-    assert response.status_code == status.HTTP_201_CREATED
-
-
-@pytest.mark.parametrize("nulled_fields", enumerate(FIELDS_CONTRIBUICAO["optional"]))
-def test_create_plano_trabalho_contribuicao_null_optional_fields(
-    # fixture example_pe é necessária para cumprir IntegrityConstraint (FK)
-    truncate_pe,  # pylint: disable=unused-argument
-    example_pe,  # pylint: disable=unused-argument
-    input_pt: dict,
-    nulled_fields: list,
-    user1_credentials: dict,
-    header_usr_1: dict,
-    client: Client,
-):
-    """Tenta criar um novo plano de trabalho enviando null nos campos opcionais"""
-    partial_input_pt = input_pt.copy()
-    offset, field_list = nulled_fields
-    for field in field_list:
-        for contribuicao in partial_input_pt["contribuicoes"]:
-            if field in contribuicao:
-                contribuicao[field] = None
-
-    partial_input_pt["id_plano_trabalho"] = 557 + offset
-    response = client.put(
-        f"/organizacao/SIAPE/{user1_credentials['cod_unidade_autorizadora']}"
-        f"/plano_trabalho/{partial_input_pt['id_plano_trabalho']}",
-        json=partial_input_pt,
-        headers=header_usr_1,
-    )
-    assert response.status_code == status.HTTP_201_CREATED
-
-
 @pytest.mark.parametrize(
     "missing_fields", enumerate(FIELDS_PLANO_TRABALHO["mandatory"])
 )
@@ -686,6 +628,102 @@ def test_create_pt_invalid_tipo_contribuicao(
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
         detail_message = "Tipo de contribuição inválida; permitido: 1 a 3"
         assert_error_message(response, detail_message)
+
+
+class TestPercentualContribuicao(BasePTTest):
+    """Testes para verificar a validação do percentual de contribuição no
+    Plano de Trabalho."""
+
+    @pytest.mark.parametrize("percentual_contribuicao", [
+        -10,
+        0,
+        50,
+        100,
+        110
+    ])
+    def test_create_plano_trabalho_percentual_contribuicao(
+        self,
+        percentual_contribuicao: int,
+    ):
+        """Testa a criação de um Plano de Trabalho com diferentes valores
+        de percentual de contribuição.
+
+        Args:
+            percentual_contribuicao (int): Valor do percentual de
+                contribuição a ser testado.
+        """
+        input_pt = self.input_pt.copy()
+        input_pt["contribuicoes"][0]["percentual_contribuicao"] = percentual_contribuicao
+        input_pt["contribuicoes"][1]["percentual_contribuicao"] = percentual_contribuicao
+
+        response = self.create_pt(input_pt, header_usr=self.header_usr_1)
+
+        if 0 <= percentual_contribuicao <= 100:
+            assert response.status_code == status.HTTP_201_CREATED
+        else:
+            assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+            assert_error_message(
+                response,
+                "O percentual de contribuição deve estar entre 0 e 100."
+            )
+
+
+@pytest.mark.parametrize("omitted_fields", enumerate(FIELDS_CONTRIBUICAO["optional"]))
+def test_create_plano_trabalho_contribuicao_omit_optional_fields(
+    # fixture example_pe é necessária para cumprir IntegrityConstraint (FK)
+    truncate_pe,  # pylint: disable=unused-argument
+    example_pe,  # pylint: disable=unused-argument
+    input_pt: dict,
+    omitted_fields: list,
+    user1_credentials: dict,
+    header_usr_1: dict,
+    client: Client,
+):
+    """Tenta criar um novo plano de trabalho omitindo campos opcionais"""
+    partial_input_pt = input_pt.copy()
+    offset, field_list = omitted_fields
+    for field in field_list:
+        for contribuicao in partial_input_pt["contribuicoes"]:
+            if field in contribuicao:
+                del contribuicao[field]
+
+    partial_input_pt["id_plano_trabalho"] = 557 + offset
+    response = client.put(
+        f"/organizacao/SIAPE/{user1_credentials['cod_unidade_autorizadora']}"
+        f"/plano_trabalho/{partial_input_pt['id_plano_trabalho']}",
+        json=partial_input_pt,
+        headers=header_usr_1,
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.parametrize("nulled_fields", enumerate(FIELDS_CONTRIBUICAO["optional"]))
+def test_create_plano_trabalho_contribuicao_null_optional_fields(
+    # fixture example_pe é necessária para cumprir IntegrityConstraint (FK)
+    truncate_pe,  # pylint: disable=unused-argument
+    example_pe,  # pylint: disable=unused-argument
+    input_pt: dict,
+    nulled_fields: list,
+    user1_credentials: dict,
+    header_usr_1: dict,
+    client: Client,
+):
+    """Tenta criar um novo plano de trabalho enviando null nos campos opcionais"""
+    partial_input_pt = input_pt.copy()
+    offset, field_list = nulled_fields
+    for field in field_list:
+        for contribuicao in partial_input_pt["contribuicoes"]:
+            if field in contribuicao:
+                contribuicao[field] = None
+
+    partial_input_pt["id_plano_trabalho"] = 557 + offset
+    response = client.put(
+        f"/organizacao/SIAPE/{user1_credentials['cod_unidade_autorizadora']}"
+        f"/plano_trabalho/{partial_input_pt['id_plano_trabalho']}",
+        json=partial_input_pt,
+        headers=header_usr_1,
+    )
+    assert response.status_code == status.HTTP_201_CREATED
 
 
 # Avaliação de registros de execução
