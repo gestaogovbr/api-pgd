6303be7a811f7a7d68f49c7c1c9d3a188ff47127
Augusto Herrmann <augusto+github@herrmann.tech>
2024-06-07 18:13:04 -0300

    Refactor into a class test for value of avaliacao

--- tests/plano_trabalho/avaliacao_registros_execucao_test.py
+++ tests/plano_trabalho/avaliacao_registros_execucao_test.py
@@ -7,7 +7,7 @@ from httpx import Client, status
 
 from util import assert_error_message
 
-from .core_test import FIELDS_AVALIACAO_REGISTROS_EXECUCAO
+from .core_test import BasePTTest, FIELDS_AVALIACAO_REGISTROS_EXECUCAO
 
 
 class TestCreatePTMissingMandatoryFieldsAvaliacaoRegistrosExecucao(BasePTTest):
@@ -29,7 +29,7 @@ class TestCreatePTMissingMandatoryFieldsAvaliacaoRegistrosExecucao(BasePTTest):
         registros de execução está faltando.
         """
         input_pt = self.input_pt.copy()
-        offset, field_list = omitted_fields
+        _, field_list = omitted_fields
         for field in field_list:
             input_pt["avaliacao_registros_execucao"][0][field] = None
 
@@ -38,15 +38,7 @@ class TestCreatePTMissingMandatoryFieldsAvaliacaoRegistrosExecucao(BasePTTest):
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 
-@pytest.mark.parametrize("avaliacao_registros_execucao", [0, 1, 2, 5, 6])
-def test_create_pt_invalid_avaliacao_registros_execucao(
-    input_pt: dict,
-    user1_credentials: dict,
-    header_usr_1: dict,
-    truncate_pt,  # pylint: disable=unused-argument
-    avaliacao_registros_execucao: int,
-    client: Client,
-):
+class TestCreatePTInvalidAvaliacaoRegistrosExecucao(BasePTTest):
     """Testa a criação de um plano de trabalho com um valor inválido para
     o campo avaliacao_registros_execucao.
 
@@ -59,20 +51,25 @@ def test_create_pt_invalid_avaliacao_registros_execucao(
     e que a mensagem de erro "Avaliação de registros de execução
     inválida; permitido: 1 a 5" esteja presente na resposta.
     """
-    input_pt["avaliacao_registros_execucao"][0][
-        "avaliacao_registros_execucao"
-    ] = avaliacao_registros_execucao
 
-    response = client.put(
-        f"/organizacao/SIAPE/{user1_credentials['cod_unidade_autorizadora']}"
-        f"/plano_trabalho/{input_pt['id_plano_trabalho']}",
-        json=input_pt,
-        headers=header_usr_1,
-    )
+    @pytest.mark.parametrize("avaliacao_registros_execucao", [0, 1, 2, 5, 6])
+    def test_create_pt_invalid_avaliacao_registros_execucao(
+        self,
+        avaliacao_registros_execucao: int,
+    ):
+        """Testa a criação de um plano de trabalho com um valor inválido para
+        o campo avaliacao_registros_execucao.
+        """
+        input_pt = self.input_pt.copy()
+        input_pt["avaliacao_registros_execucao"][0][
+            "avaliacao_registros_execucao"
+        ] = avaliacao_registros_execucao
 
-    if avaliacao_registros_execucao in range(1, 6):
-        assert response.status_code == status.HTTP_201_CREATED
-    else:
-        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-        detail_message = "Avaliação de registros de execução inválida; permitido: 1 a 5"
-        assert_error_message(response, detail_message)
+        response = self.create_pt(input_pt)
+
+        if avaliacao_registros_execucao in range(1, 6):
+            assert response.status_code == status.HTTP_201_CREATED
+        else:
+            assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+            detail_message = "Avaliação de registros de execução inválida; permitido: 1 a 5"
+            assert_error_message(response, detail_message)
