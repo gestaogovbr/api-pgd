3750fb874052a7c6db052509421982b522edf6fb
Augusto Herrmann <augusto+github@herrmann.tech>
2024-06-07 18:09:11 -0300

    Move tests for avaliacao_registro_execucao to its own module

--- tests/plano_trabalho/core_test.py
+++ tests/plano_trabalho/core_test.py
@@ -556,49 +556,6 @@ def test_create_pt_invalid_carga_horaria_disponivel(
     assert_error_message(response, detail_message)
 
 
-# Avaliação de registros de execução
-
-
-@pytest.mark.parametrize("avaliacao_registros_execucao", [0, 1, 2, 5, 6])
-def test_create_pt_invalid_avaliacao_registros_execucao(
-    input_pt: dict,
-    user1_credentials: dict,
-    header_usr_1: dict,
-    truncate_pt,  # pylint: disable=unused-argument
-    avaliacao_registros_execucao: int,
-    client: Client,
-):
-    """Testa a criação de um plano de trabalho com um valor inválido para
-    o campo avaliacao_registros_execucao.
-
-    O teste envia uma requisição PUT para a rota
-    "/organizacao/SIAPE/{cod_unidade_autorizadora}/plano_trabalho/{id_plano_trabalho}"
-    com diferentes valores campo avaliacao_registros_execucao.
-    Quando o valor for válido (entre 1 e 5), espera-se que a resposta
-    tenha o status HTTP 201 Created. Quando o valor for inválido,
-    espera-se que a resposta tenha o status HTTP 422 Unprocessable Entity
-    e que a mensagem de erro "Avaliação de registros de execução
-    inválida; permitido: 1 a 5" esteja presente na resposta.
-    """
-    input_pt["avaliacao_registros_execucao"][0][
-        "avaliacao_registros_execucao"
-    ] = avaliacao_registros_execucao
-
-    response = client.put(
-        f"/organizacao/SIAPE/{user1_credentials['cod_unidade_autorizadora']}"
-        f"/plano_trabalho/{input_pt['id_plano_trabalho']}",
-        json=input_pt,
-        headers=header_usr_1,
-    )
-
-    if avaliacao_registros_execucao in range(1, 6):
-        assert response.status_code == status.HTTP_201_CREATED
-    else:
-        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-        detail_message = "Avaliação de registros de execução inválida; permitido: 1 a 5"
-        assert_error_message(response, detail_message)
-
-
 @pytest.mark.parametrize(
     "cpf_participante",
     [
