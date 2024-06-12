558fff7629f6162b896417e59bca72147c8f602b
Augusto Herrmann <augusto+github@herrmann.tech>
2024-06-12 12:01:37 -0300

    Fix usage of model_validator

--- src/schemas.py
+++ src/schemas.py
@@ -311,7 +311,7 @@ class EntregaSchema(BaseModel):
             raise ValueError("Meta de entrega deve ser um valor positivo")
         return meta_entrega
 
-    @field_validator("meta_entrega", mode="before")
+    @model_validator(mode="after")
     def validate_meta_percentual(self, meta_entrega: int) -> int:
         if (
             meta_entrega is not None
