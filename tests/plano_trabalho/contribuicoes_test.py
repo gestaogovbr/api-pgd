6333a359b9bc0ad0a41810955e057f75f96d0989
Augusto Herrmann <augusto+github@herrmann.tech>
2024-06-28 10:05:00 -0300

    Handle optional fields for contribuicao in tests

--- tests/plano_trabalho/contribuicoes_test.py
+++ tests/plano_trabalho/contribuicoes_test.py
@@ -136,6 +136,8 @@ class TestCreatePTOmitOptionalFields(BasePTTest):
         offset, field_list = omitted_fields
         for field in field_list:
             for contribuicao in partial_input_pt["contribuicoes"]:
+                # torna os opcionais campos id_plano_entregas e id_entrega
+                contribuicao["tipo_contribuicao"] = 3
                 if field in contribuicao:
                     del contribuicao[field]
 
