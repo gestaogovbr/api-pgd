e74ce032afdb5d9c249906ca197080f3aa349641
Augusto Herrmann <augusto+github@herrmann.tech>
2024-08-13 17:16:53 -0300

    Refactor into classes permissions tests for P.E.

--- tests/plano_entregas/permissions_test.py
+++ tests/plano_entregas/permissions_test.py
@@ -2,63 +2,64 @@
 Entregas.
 """
 
-from httpx import Client
 from fastapi import status as http_status
 
 from .core_test import BasePETest
 
 
-def test_get_plano_entregas_different_unit(
-    truncate_pe,  # pylint: disable=unused-argument
-    example_pe_unidade_3,  # pylint: disable=unused-argument
-    header_usr_2: dict,
-    input_pe,
-    client: Client,
-):
-    """Tenta buscar um plano de entregas existente em uma unidade diferente,
-    à qual o usuário não tem acesso."""
+class TestPermissionsGetPE(BasePETest):
+    """Testes relacionados a permissões e acesso ao Plano de Entregas de diferentes unidades."""
 
-    response = client.get(
-        f"/organizacao/SIAPE/3"  # Sem autorização nesta unidade
-        f"/plano_entregas/{input_pe['id_plano_entregas']}",
-        headers=header_usr_2,
-    )
-    assert response.status_code == http_status.HTTP_403_FORBIDDEN
+    def test_get_plano_entregas_different_unit(
+        self,
+        truncate_pe,  # pylint: disable=unused-argument
+        example_pe_unidade_3,  # pylint: disable=unused-argument
+        header_usr_2: dict,
+    ):
+        """Tenta buscar um plano de entregas existente em uma unidade diferente,
+        à qual o usuário não tem acesso.
+        """
+        input_pe = self.input_pe.copy()
+        response = self.get_plano_entregas(
+            input_pe["id_plano_entregas"],
+            3,  # Sem autorização nesta unidade
+            header_usr=header_usr_2,
+        )
+        assert response.status_code == http_status.HTTP_403_FORBIDDEN
 
+    def test_get_plano_entregas_different_unit_admin(
+        self,
+        truncate_pe,  # pylint: disable=unused-argument
+        header_admin: dict,
+        admin_credentials: dict,
+    ):
+        """Tenta, como administrador, criar um novo Plano de Entregas
+        em uma organização diferente da sua própria organização.
+        """
+        input_pe = self.input_pe.copy()
+        input_pe["cod_unidade_autorizadora"] = 3  # Unidade diferente
 
-def test_get_plano_entregas_different_unit_admin(
-    truncate_pe,  # pylint: disable=unused-argument
-    input_pe: dict,
-    header_admin: dict,
-    admin_credentials: dict,
-    client: Client,
-):
-    """Tenta, como administrador, criar um novo Plano de Entregas em uma
-    organização diferente da sua própria organização.
-    """
-    input_pe["cod_unidade_autorizadora"] = 3  # unidade diferente
+        response = self.client.get(
+            f"/user/{admin_credentials['username']}",
+            headers=header_admin,
+        )
 
-    response = client.get(
-        f"/user/{admin_credentials['username']}",
-        headers=header_admin,
-    )
+        # Verifica se o usuário é admin e se está em outra unidade
+        assert response.status_code == http_status.HTTP_200_OK
+        admin_data = response.json()
+        assert (
+            admin_data.get("cod_unidade_autorizadora", None)
+            != input_pe["cod_unidade_autorizadora"]
+        )
+        assert admin_data.get("is_admin", None) is True
 
-    # Verifica se o usuário é admin e se está em outra unidade
-    assert response.status_code == http_status.HTTP_200_OK
-    admin_data = response.json()
-    assert (
-        admin_data.get("cod_unidade_autorizadora", None)
-        != input_pe["cod_unidade_autorizadora"]
-    )
-    assert admin_data.get("is_admin", None) is True
+        response = self.create_plano_entregas(
+            input_pe,
+            cod_unidade_autorizadora=input_pe["cod_unidade_autorizadora"],
+            id_plano_entregas=input_pe["id_plano_entregas"],
+            header_usr=header_admin,
+        )
 
-    response = client.put(
-        f"/organizacao/SIAPE/{input_pe['cod_unidade_autorizadora']}"
-        f"/plano_entregas/{input_pe['id_plano_entregas']}",
-        json=input_pe,
-        headers=header_admin,
-    )
-
-    assert response.status_code == http_status.HTTP_201_CREATED
-    assert response.json().get("detail", None) is None
-    BasePETest.assert_equal_plano_entregas(response.json(), input_pe)
+        assert response.status_code == http_status.HTTP_201_CREATED
+        assert response.json().get("detail", None) is None
+        self.assert_equal_plano_entregas(response.json(), input_pe)
