fab1cd17d97242c4e04c104c9e979218c8154b68
Augusto Herrmann <augusto+github@herrmann.tech>
2024-10-03 14:57:29 -0300

    Fix wrong argument name

--- tests/endpoints_test.py
+++ tests/endpoints_test.py
@@ -6,11 +6,14 @@ from httpx import Client
 from fastapi import status
 
 
+# Entrypoint da API e redirecionamentos
+
+
 def test_redirect_to_docs_html(client: Client):
     """
     Testa se o acesso por um navegador na raiz redireciona para o /docs.
     """
-    response = client.get("/", allow_redirects=False, headers={"Accept": "text/html"})
+    response = client.get("/", follow_redirects=False, headers={"Accept": "text/html"})
 
     assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
     assert response.headers["Location"] == "/docs"
@@ -21,7 +24,7 @@ def test_redirect_to_entrypoint_json(client: Client):
     Testa se o acesso por um script na raiz redireciona para o /openapi.json.
     """
     response = client.get(
-        "/", allow_redirects=False, headers={"Accept": "application/json"}
+        "/", follow_redirects=False, headers={"Accept": "application/json"}
     )
 
     assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
