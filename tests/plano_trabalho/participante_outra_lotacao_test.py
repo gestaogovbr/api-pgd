bc89c9d10ce377e61adf500c89a6c959dbc0b804
Augusto Herrmann <augusto+github@herrmann.tech>
2024-10-16 16:56:56 -0300

    Apply black formatter

--- tests/plano_trabalho/participante_outra_lotacao_test.py
+++ tests/plano_trabalho/participante_outra_lotacao_test.py
@@ -9,6 +9,7 @@ from util import assert_error_message
 
 from .core_test import BasePTTest
 
+
 class TestCreatePlanoTrabalhoParticipanteOutraUnidade(BasePTTest):
     """Testes para verificar a criação de um Plano de Trabalho
     vinculado a um Participante, onde o cod_unidade_executora difere
@@ -17,6 +18,7 @@ class TestCreatePlanoTrabalhoParticipanteOutraUnidade(BasePTTest):
     Segundo regra de negócio, um Participante pode ser lotado em uma
     unidade e executar um Plano de Trabalho em outra unidade.
     """
+
     def test_create_plano_trabalho_participante_outra_unidade(self):
         """Cria um Participante, e então cria um Plano de Trabalho
         vinculado a ele em outra unidade. Usa cod_unidade_lotacao
@@ -27,7 +29,7 @@ class TestCreatePlanoTrabalhoParticipanteOutraUnidade(BasePTTest):
         # cod_unidade_lotacao do Participante criado pela fixture example_part
         # é 99
         # cod_unidade_executora original do input_part é 99
-        input_pt['cod_unidade_executora'] = 101
+        input_pt["cod_unidade_executora"] = 101
 
         # cria o Plano de Trabalho
         response = self.put_plano_trabalho(
@@ -38,7 +40,7 @@ class TestCreatePlanoTrabalhoParticipanteOutraUnidade(BasePTTest):
         response_data = response.json()
         self.assert_equal_plano_trabalho(response_data, input_pt)
         # verifica se a resposta veio com o cod_unidade_executora esperado
-        assert response_data['cod_unidade_executora'] == 101
+        assert response_data["cod_unidade_executora"] == 101
 
         # Consulta API para conferir se a criação foi persistida
         response = self.get_plano_trabalho(
@@ -52,4 +54,4 @@ class TestCreatePlanoTrabalhoParticipanteOutraUnidade(BasePTTest):
         # verifica se os dados gravados são iguais aos informados
         self.assert_equal_plano_trabalho(response_data, input_pt)
         # verifica o cod_unidade_executora
-        assert response_data['cod_unidade_executora'] == 101
+        assert response_data["cod_unidade_executora"] == 101
