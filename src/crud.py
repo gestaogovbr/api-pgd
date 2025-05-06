3dd19116468ae2648fab02af6953d9d24701379e
Eduardo Lauer <edulauer@hotmail.com>
2025-05-06 14:12:01 -0300

    remove data_assinatura_tcr validation if greater than data_inicio

--- src/crud.py
+++ src/crud.py
@@ -164,13 +164,6 @@ async def create_plano_trabalho(
                 f" matricula_siape: {plano_trabalho.matricula_siape}\n"
                 f" cod_unidade_lotacao: {plano_trabalho.cod_unidade_lotacao_participante}"
             )
-        if plano_trabalho.data_inicio < db_participante.data_assinatura_tcr:
-            raise ValueError(
-                "data_inicio do Plano de Trabalho deve ser maior ou igual à "
-                "data_assinatura_tcr do Participante.\n"
-                f" data_inicio: {plano_trabalho.data_inicio}\n"
-                f" data_assinatura_tcr: {db_participante.data_assinatura_tcr}"
-            )
         session.add(db_participante)
         db_participante.planos_trabalho.append(db_plano_trabalho)
 
